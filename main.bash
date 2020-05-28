6bea3eff726d8aacdbdd7ef18a3a0022c4656b57
Michael Murphy <murphym18@gmail.com>
2020-05-28 02:08:28 -0700

    Fix input argument

--- main.bash
+++ main.bash
@@ -4,7 +4,7 @@ PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function pretty_print_gfm_markdown() {
     MARKDOWN_FILE="$1"
-    /usr/bin/pandoc "--input=$MARKDOWN_FILE" "--output=-" --from=gfm --to=plain
+    /usr/bin/pandoc "$MARKDOWN_FILE" "--output=-" --from=gfm --to=plain
 }
 
 pretty_print_gfm_markdown "$PROJECT_PATH/README.md"
