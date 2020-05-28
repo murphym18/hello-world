bd799e3b2f40f2866807fe341947f7cb27e01810
Michael Murphy <murphym18@users.noreply.github.com>
2020-05-28 01:45:19 -0700

    Add function to print markdown file

--- main.bash
+++ main.bash
@@ -2,4 +2,9 @@
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+function pretty_print_gfm_markdown() {
+    MARKDOWN_FILE="$1"
+    /usr/bin/pandoc "--input=$MARKDOWN_FILE" "--output=-" --from=gfm --to=plain
+}
+
 cat "$PROJECT_PATH/README.md"
